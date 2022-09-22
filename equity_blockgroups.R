add80b4f3f21be88d53d5b1f81284a361bca31fd
jensenmj <mjensen@psrc.org>
2022-09-21 18:03:44 -0700

    Delivery to Elmer.

--- equity_blockgroups.R
+++ equity_blockgroups.R
@@ -12,6 +12,17 @@ using <- function(...) {                                                        
   }
 }
 
+elmer_connect <-function(){dbConnect(odbc::odbc(),
+                                     driver = "ODBC Driver 17 for SQL Server",
+                                     server = "AWS-PROD-SQL\\Sockeye",
+                                     database = "Elmer",
+                                     trusted_connection = "yes")}
+
+run_query <- function(conn, send_sql){
+  rs <- dbSendQuery(conn, SQL(send_sql))
+  dbClearResult(rs)
+}
+
 vars <- c("B02001_001","B02001_002",                                                               # ACS variables
           "C17002_001","C17002_008",
           "B22010_001","B22010_004","B22010_007",
@@ -28,22 +39,35 @@ label_quintile <- function(var){
 
 # --------------------------------------------------------
 
-using("magrittr","data.table", "tidycensus")                                                       # Libraries
+using("magrittr","data.table", "tidycensus","odbc","DBI")                                          # Libraries
 
-equity_shares <- get_acs(geography="block group", variables=vars, state=53,
+blockgroup_shares <- get_acs(geography="block group", variables=vars, state=53,
                          county=c("033","035","053","061"), year=dyear, survey="acs5") %>%
   setDT %>% dcast(GEOID ~ variable, value.var="estimate") %>%
-   .[B02001_001 > 0,`:=`(POC_share= (B02001_001 - B02001_002) / B02001_001,                        # Calculate share per block group
-                         Income_share= (C17002_001 - C17002_008) / C17002_001,
-                         Disability_share= (B22010_001-B22010_004-B22010_007) / B22010_001,
-                         Youth_share= B11005_002 / B11005_001,
-                         Older_share= B11007_002 / B11007_001,
-                         LEP_share= (C16002_004 + C16002_007 + C16002_010 + C16002_013) / C16002_001)] %>%
+   .[B02001_001 > 0,`:=`(Year=..dyear, poc_share= (B02001_001 - B02001_002) / B02001_001,          # Calculate share per block group
+                         income_share= (C17002_001 - C17002_008) / C17002_001,
+                         disability_share= (B22010_001-B22010_004-B22010_007) / B22010_001,
+                         youth_share= B11005_002 / B11005_001,
+                         older_share= B11007_002 / B11007_001,
+                         lep_share= (C16002_004 + C16002_007 + C16002_010 + C16002_013) / C16002_001)] %>%
    .[, which(grepl("\\d$", colnames(.))):=NULL] %>% .[complete.cases(.)]                           # Remove original columns & NA rows
-equity_dims   <- c("POC","Income","Disability","Youth","Older","LEP")
+equity_dims   <- c("poc","income","disability","youth","older","lep")
 share_vars <- paste0(equity_dims,"_share")
 quintile_vars <- paste0(equity_dims,"_quintile")
-equity_shares[, (quintile_vars):=lapply(.SD, label_quintile), .SDcols=share_vars]                  # Add quintile label
+blockgroup_shares[, (quintile_vars):=lapply(.SD, label_quintile), .SDcols=share_vars]              # Add quintile value
+setnames(blockgroup_shares, old=c("GEOID","Year"), new=c("geoid", "data_year"))
 
-filename <- paste0(getwd(),"/equity_shares.csv")
-fwrite(equity_shares, filename)                                                                    # Write to file
+#filename <- paste0(getwd(),"/equity_shares.csv")
+#fwrite(equity_shares, filename)                                                                   # Write to file
+
+sockeye_connection <- elmer_connect()
+table_id <- Id(schema = "stg", table = "blockgroup_shares")
+dbWriteTable(sockeye_connection, table_id, blockgroup_shares, overwrite = TRUE)                    # Write staging table
+merge_sql <- paste("MERGE INTO equity.blockgroup_shares WITH (HOLDLOCK) AS target",                # Insert new data
+                   "USING stg.blockgroup_shares AS source",
+                   "ON target.data_year=source.data_year AND target.geoid=source.geoid",
+                   "WHEN NOT MATCHED BY TARGET THEN INSERT (", paste0(colnames(blockgroup_shares), collapse=", "),")",
+                   "VALUES (", paste0("source.", colnames(blockgroup_shares), collapse=", "), ");")
+run_query(sockeye_connection, merge_sql)                                                           # Execute the query
+run_query(sockeye_connection, "DROP TABLE stg.blockgroup_shares")                                  # Clean up
+dbDisconnect(sockeye_connection)
