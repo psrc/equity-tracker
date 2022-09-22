8e01c4c7904f7c932d94699436aed37c3abc268e
Brice Nichols <bricenichols@gmail.com>
2022-09-22 15:18:47 -0700

    using elmer table for equity quintile geography, updated csv output file

--- distance_indicator.py
+++ distance_indicator.py
@@ -3,40 +3,32 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 import sqlalchemy
-import pyodbc
 from shapely import wkt
 from scipy.spatial import cKDTree
 from shapely.geometry import Point
+from sqlalchemy.engine import URL
+from pymssql import connect
+import warnings
+warnings.filterwarnings('ignore')
 
-def read_from_sde(connection_string, feature_class_name, version,
-                  crs='EPSG:2285', cols=None):
+# Define which year to use for equity quintile definitions (from Elmer)
+# As of Sept 2022, options are 2019, 2020
+equity_quintiles_year = 2020
 
+def load_elmer_geo_table(feature_class_name, con, crs):
+    """ Load ElmerGeo table as geoDataFrame, applying a specified coordinate reference system (CRS)
     """
-    Returns the specified feature class as a geodataframe from ElmerGeo.
-    
-    Parameters
-    ----------
-    connection_string : SQL connection string that is read by geopandas 
-                        read_sql function
-    
-    feature_class_name: the name of the featureclass in PSRC's ElmerGeo 
-                        Geodatabase
-    
-    cs: cordinate system
-    """
-    engine = sqlalchemy.create_engine(connection_string)
-    con=engine.connect()
-    df = pd.read_sql('select *, Shape.STAsText() as geometry from %s' % 
-                (feature_class_name), con=con)
+    geo_col_stmt = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME=" + "\'" + feature_class_name + "\'" + " AND DATA_TYPE='geometry'"
+    geo_col = str(pd.read_sql(geo_col_stmt, con).iloc[0,0])
+    query_string = 'SELECT *,' + geo_col + '.STGeometryN(1).ToString()' + ' FROM ' + feature_class_name
 
+    df = pd.read_sql(query_string, con)
+    df.rename(columns={'':'geometry'}, inplace = True)
     df['geometry'] = df['geometry'].apply(wkt.loads)
+
     gdf = gpd.GeoDataFrame(df, geometry='geometry')
     gdf.crs = crs
-    if not cols:
-        cols = [col for col in gdf.columns if col not in 
-            ['Shape', 'GDB_GEOMATTR_DATA', 'SDE_STATE_ID']]
-    gdf = gdf[cols]
-    
+
     return gdf
 
 def find_nearest(gdA, gdB):
@@ -84,49 +76,54 @@ def weighted_avg(df, val_col, wt_col, agg_col):
 def main():
      
     # Define SQL connections for ElmerGeo
-    elmergeo_conn_string = 'mssql+pyodbc://AWS-PROD-SQL\Sockeye/ElmerGeo?driver=SQL Server?Trusted_Connection=yes'
     crs = 'EPSG:2285'
-    version = "'DBO.Default'"
+    elmergeo_conn_string = 'AWS-Prod-SQL\Sockeye'
+    elmergeo_con = connect('AWS-Prod-SQL\Sockeye', database="ElmerGeo")
 
     # Connections to Elmer
     elmer_conn_string = "DRIVER={ODBC Driver 17 for SQL Server}; SERVER=AWS-PROD-SQL\Sockeye; DATABASE=Elmer; trusted_connection=yes"
-    sql_conn = pyodbc.connect(elmer_conn_string)
+    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": elmer_conn_string})
+    elmer_engine = sqlalchemy.create_engine(connection_url)
+
+    # Load equity quintiles definitions and join to block groups
+    equity_shares_df = pd.read_sql(sql='select * from equity.v_blockgroup_shares', con=elmer_engine)
+    equity_shares_df = equity_shares_df[equity_shares_df['data_year'] == equity_quintiles_year]
+    #equity_shares_df = pd.read_csv(r'Y:\Equity Indicators\access\equity_shares.csv')
 
     # Load parcel data
     print('Loading parcel points...')
-    parcels_gdf = read_from_sde(elmergeo_conn_string, 'parcels_urbansim_2018_pts', version, crs=crs)
+    parcels_gdf = load_elmer_geo_table('parcels_urbansim_2018_pts', elmergeo_con, crs)
+    #parcels_gdf = read_from_sde(elmergeo_conn_string, 'parcels_urbansim_2018_pts', version, crs=crs)
     parcels_gdf = parcels_gdf[['parcel_id','geometry']]
 
     # Load HCT location data
     print('Loading HCT geospatial data...')
-    hct_gdf = read_from_sde(elmergeo_conn_string, 'hct_station_areas', version, crs=crs)
+    hct_gdf = load_elmer_geo_table('hct_station_areas', elmergeo_con, crs)
     hct_gdf.geometry = hct_gdf.centroid
 
     # Load 2020 Census block groups
     # FIXME: if layers don't load, wait and try again
     print('Loading Census data...')
-    block_grp_gdf = read_from_sde(elmergeo_conn_string, 'blockgrp2020', version, crs=crs)
+    block_grp_gdf = load_elmer_geo_table('blockgrp2020', elmergeo_con, crs)
     block_grp_gdf = block_grp_gdf[['geoid20','geometry']]
     print('Overlaying parcels on Census data...')
     parcels_gdf = gpd.overlay(parcels_gdf, block_grp_gdf)
 
     # Use households per parcel as a weight
     # FIXME: use Elmer when data is available
-    #R:\e2projects_two\SoundCast\Inputs\dev\landuse\2018\v3_RTP
     print('Loading parcel household data...')
     df_parcel_hh = pd.read_csv(r'Y:\Equity Indicators\access\parcels_urbansim.txt', 
                                sep=' ', usecols=['parcelid','hh_p'])
     parcels_gdf = parcels_gdf.merge(df_parcel_hh, left_on='parcel_id', right_on='parcelid')
 
-    # Load equity quintiles definitions and join to block groups
-    # # FIXME: run directly from the R Script? 
-    equity_shares_df = pd.read_csv(r'Y:\Equity Indicators\access\equity_shares.csv')
-    parcels_gdf['GEOID'] = parcels_gdf['geoid20'].astype('int64')
-    parcels_gdf = parcels_gdf.merge(equity_shares_df, on='GEOID', how='left')
+    # Merge equity share geographies
+    parcels_gdf['geoid'] = parcels_gdf['geoid20'].astype('int64')
+    equity_shares_df['geoid'] = equity_shares_df['geoid'].astype('int64')
+    parcels_gdf = parcels_gdf.merge(equity_shares_df, on='geoid', how='left')
 
     # Drop any null rows
-    print("Removed %s null of %d parcels" %(len(parcels_gdf[parcels_gdf['Older_quintile'].isnull()]),len(parcels_gdf)))
-    parcels_gdf = parcels_gdf[~parcels_gdf['Older_quintile'].isnull()]
+    print("Removed %s null of %d parcels" %(len(parcels_gdf[parcels_gdf['older_quintile'].isnull()]),len(parcels_gdf)))
+    parcels_gdf = parcels_gdf[~parcels_gdf['older_quintile'].isnull()]
 
     #######################################################
     # Access to Transit Stations
@@ -149,8 +146,8 @@ def main():
 
     # Iterate over measures and submodes
     full_output_df = pd.DataFrame()
-    for agg_col in ['POC_quintile', 'Income_quintile', 'Disability_quintile',
-                    'Youth_quintile', 'Older_quintile', 'LEP_quintile']:
+    for agg_col in ['poc_quintile', 'income_quintile', 'disability_quintile',
+                    'youth_quintile', 'older_quintile', 'lep_quintile']:
         for submode in ['all_hct','light_rail','commuter_rail','ferry','passenger_ferry','brt']:
             df = weighted_avg(results_dict[submode], 'miles', 'hh_p', agg_col)
             df = df.reset_index()
@@ -159,20 +156,11 @@ def main():
             df['aggregation'] = agg_col.split('_')[0]
             full_output_df = pd.concat([full_output_df,df.reset_index()])
 
-    # add label for qunitle
-    quintile_def = {
-        1: 'Low',
-        2: 'Low Medium',
-        3: 'Medium',
-        4: 'Medium High',
-        5: 'High'}
-    full_output_df['quintile'] = full_output_df['quintile'].astype('int').map(quintile_def)
-
     #add a label to wt_avg_miles
     full_output_df.rename(columns={'wt_avg': 'wt_avg_miles'}, inplace=True)
 
     # Write to local dir
-    full_output_df.to_csv(r'distance_indicator.csv', index=False)
+    full_output_df[['aggregation','quintile','access_to','wt_avg_miles']].to_csv(r'distance_indicator.csv', index=False)
 
 if __name__ == '__main__':
     start_time = time.time()
