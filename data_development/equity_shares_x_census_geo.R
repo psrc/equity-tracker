2885430eb5e94ab621d868b5ced83d46ed12a72f
jensenmj <mjensen@psrc.org>
2023-03-28 12:29:33 -0700

    Shift to psrcelmer; additional comment.

--- data_development/equity_shares_x_census_geo.R
+++ data_development/equity_shares_x_census_geo.R
@@ -1,33 +1,17 @@
+library(magrittr)
+library(tidycensus)
+library(psrcelmer)
+library(data.table)
 
 # Functions ----------------------------------------------
 
-using <- function(...) {                                                                           # Attaches libraries; installs if needed
-  libs <- unlist(list(...))
-  req  <- unlist(lapply(libs,require,character.only=TRUE))
-  need <- libs[req==FALSE]
-  if(length(need)>0){
-    install.packages(need)
-    lapply(need,require,character.only=TRUE)
-  }
-}
-
-elmer_connect <-function(){dbConnect(odbc::odbc(),
-                                     driver = "ODBC Driver 17 for SQL Server",
-                                     server = "AWS-PROD-SQL\\Sockeye",
-                                     database = "Elmer",
-                                     trusted_connection = "yes")}
-
-run_query <- function(conn, send_sql){
-  rs <- dbSendQuery(conn, SQL(send_sql))
-  dbClearResult(rs)
-}
-
-vars <- c("B02001_001","B02001_002",                                                               # ACS variables
-          "C17002_001","C17002_008",
-          "B22010_001","B22010_004","B22010_007",
-          "B11005_001","B11005_002",
-          "B11007_001","B11007_002",
-          "C16002_001","C16002_004","C16002_007","C16002_010","C16002_013")
+# ACS variables
+vars <- c("B02001_001","B02001_002",                                                               # POC / race
+          "C17002_001","C17002_008",                                                               # income
+          "B22010_001","B22010_004","B22010_007",                                                  # disability
+          "B11005_001","B11005_002",                                                               # presence of youth in household
+          "B11007_001","B11007_002",                                                               # presence of older in household
+          "C16002_001","C16002_004","C16002_007","C16002_010","C16002_013")                        # limited English proficiency
 
 label_quintile <- function(var){
   breakpoints <- unique(quantile(var, probs=0:5/5, na.rm=TRUE))                                    # Unique so all-zero quintiles act as one bin
@@ -36,12 +20,12 @@ label_quintile <- function(var){
   return(rv)
 }
 
-get_psrc_equity_shares <- function(dyear, scale){
-  equity_dims   <- c("poc","income","disability","youth","older","lep")
+get_psrc_equity_shares <- function(dyear, scale){                                                   # dyear - last year of 5yr ACS;
+  equity_dims   <- c("poc","income","disability","youth","older","lep")                             # scale - either "block group" or "tract"
   share_vars <- paste0(equity_dims,"_share")
   quintile_vars <- paste0(equity_dims,"_quintile")
 
-  shares <- get_acs(geography=scale, variables=vars, state=53,
+  shares <- get_acs(geography=scale, variables=vars, state=53,                                      # Retrieves the data
                     county=c("033","035","053","061"), year=dyear, survey="acs5") %>%
     setDT %>% dcast(GEOID ~ variable, value.var="estimate") %>%
     .[B02001_001 > 0,`:=`(Year=..dyear, poc_share= (B02001_001 - B02001_002) / B02001_001,          # Calculate share per block group
@@ -60,26 +44,28 @@ get_psrc_equity_shares <- function(dyear, scale){
 write_shares_to_elmer <- function(dyear, scale){
   tblname <- scale %>% gsub(" ", "", .) %>% paste0("_shares")
   rs <- get_psrc_equity_shares(dyear, scale)
-  sockeye_connection <- elmer_connect()
-  table_id <- Id(schema = "stg", table = tblname)
-  dbWriteTable(sockeye_connection, table_id, rs, overwrite = TRUE)                                 # Write staging table
-  merge_sql <- paste("MERGE INTO equity.", tblname, " WITH (HOLDLOCK) AS target",                  # Insert new data
+
+  merge_sql <- paste("MERGE INTO equity.", tblname, " WITH (HOLDLOCK) AS target",                  # Create the merge SQL syntax
                      "USING stg.", tblname, " AS source",
                      "ON target.data_year=source.data_year AND target.geoid=source.geoid",
                      "WHEN NOT MATCHED BY TARGET THEN INSERT (", paste0(colnames(rs), collapse=", "),")",
                      "VALUES (", paste0("source.", colnames(rs), collapse=", "), ");")
-  run_query(sockeye_connection, merge_sql)                                                         # Execute the query
-  run_query(sockeye_connection, paste0("DROP TABLE stg.", tblname))                                # Clean up
-  dbDisconnect(sockeye_connection)
+
+  psrcelmer::stage_table(tblname)                                                                  # Stage table first
+  psrcelmer::sql_execute(sql=merge_sql)                                                            # -- then merge
+  psrcelmer::sql_execute(sql=paste("DROP TABLE stg.", tblname))                                    # Clean up
+  return(invisible(NULL))                                                                          # No return object for this action
 }
 
 # Examples -------------------------------------------------------
-using("magrittr","data.table", "tidycensus","odbc","DBI")                                          # Libraries
+# function arguments:
+#     dyear - last year of 5yr ACS;
+#     scale - either "block group" or "tract"
 
-data_year <- 2020                                                                                  # Ending year of desired 5-yr ACS data
-
-# rs <- get_psrc_equity_shares(data_year,"block group")
+# Write to file
+# rs <- get_psrc_equity_shares(2020,"block group")
 # filename <- paste0(getwd(),"/equity_shares.csv")
-# fwrite(equity_shares, filename)                                                                  # Write to file
+# fwrite(equity_shares, filename)
 
-write_shares_to_elmer(data_year,"block group")                                                     # Write to Elmer
+# Write to Elmer
+write_shares_to_elmer(2020,"block group")
