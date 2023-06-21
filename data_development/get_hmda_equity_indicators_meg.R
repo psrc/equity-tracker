40773ca8130ac878e05ee75a6ae428e6d063e588
meggrzy <108431958+meggrzy@users.noreply.github.com>
2023-06-21 14:33:31 -0700

    not fully clean/working on plots

--- data_development/get_hmda_equity_indicators_meg.R
+++ data_development/get_hmda_equity_indicators_meg.R
@@ -2,6 +2,7 @@ library(magrittr)
 library(data.table)
 library(httr)
 library(jsonlite)
+library(psrcplot)
 
 # Helper attributes / functions -------------------------------------
 
@@ -13,7 +14,7 @@ races_vector <- c("Asian",                                                      
                   "Black or African American",
                   "2 or more minority races",
                   "White",
-                  "Joint") %>% lapply(URLencode)                                                   # Recode spaces, etc.
+                  "Joint")                                                                        # Recode spaces, etc.
 
 poc_vector <- paste0(races_vector[c(1:3,5:6,9)], collapse=",") %>% c("White") %>%
   lapply(URLencode)
@@ -22,80 +23,7 @@ ethnicities_vector <- c("Hispanic or Latino",
                        "Not Hispanic or Latino",
                        "Joint",
                        "Ethnicity Not Available",
-                       "Free Form Text Only") %>% lapply(URLencode)
-
-#sexes_vector<- c("Male","Female","Joint","Sex Not Available") %>% lapply(URLencode)
-
-#dwelling_categories_vector <- c("Single Family (1-4 Units):Site-Built",
- #                               "Multifamily:Site-Built",
-  #                              "Single Family (1-4 Units):Manufactured",
-   #                             "Multifamily:Manufactured") %>% lapply(URLencode)
-
-#income
-
-county_lookup <- data.frame(county_name=c("King","Kitsap","Pierce","Snohomish"),
-                            fips=paste0("530",c("33","35","53","61"))) %>%
-  rbind(c(county_name="Region",
-          fips=c(paste0(.$fips, collapse=",")))) %>%
-  setDT() %>% setkey(fips)
-
-hmda_url_builder <- function(dyear,
-                             actions_taken,
-                             counties="Region",
-                             races=NULL,
-                             ethnicities=NULL,
-                             sexes=NULL,
-                             dwelling_categories=NULL){
-
-  url <- paste0("https://ffiec.cfpb.gov/v2/data-browser-api/view/aggregations?counties=",
-                 county_lookup[county_name==counties, fips],
-                 "&years=", dyear, "&actions_taken=", actions_taken)                               # Multiple years not allowed here, because API sums them unless requested separately
-
-  # if(!is.null(dwelling_categories)){url %<>% paste0("&dwelling_categories=", paste0(dwelling_categories, collapse=","))}
-  # if(!is.null(sexes))              {url %<>% paste0("&sexes=",               paste0(sexes, collapse=","))}
-  # if(!is.null(ethnicities))        {url %<>% paste0("&ethnicities=",         paste0(ethnicities, collapse=","))}
-  if(!is.null(races))               {url %<>% paste0("&races=",               paste0(races, collapse=","))}
-
-  return(url)
-}
-
-hmda_api_gofer <- function(pair){                                                                  # Input is a list with 1. URL & 2. datayear; easier vectorizing than separate args
-  url    <- pair[[1]]
-  dyear  <- pair[[2]]
-  resp   <- GET(url)
-  result <- fromJSON(content(resp, "text", encoding="UTF-8"),                                      # Text is only option for this API (No json/xml)
-                     simplifyDataFrame=TRUE) %>% purrr::pluck("aggregations") %>%
-      setDT() %>% .[, year:=(dyear)]                                                               # Add column, as year isn't returned in result
-  return(result)
-}
-
-# Primary hmda function for Equity Tracker --------------------------
-
-get_hmda <- function(dyears, counties="Region",
-                     races=races_vector,
-                     ethnicities=NULL,
-                     sexes=NULL,
-                     dwelling_categories=NULL){
-  mixr <- expand.grid(dyear=dyears,                                                                # Notice the API aggregates all specified items
-                      actions_taken=c(1,3),                                                        # So years, details must be called individually
-                      actions_taken=mixr$actions_taken,
-                      races=races,
-                      ethnicities=ethnicities,
-                      sexes=sexes,
-                      dwelling_categories=dwelling_categories
-                      )
-  rs   <- data.frame(
-            mapply(hmda_url_builder,
-                   dyear=mixr$dyear,
-                   races=mixr$races),
-            mixr$dyear) %>%
-    transpose() %>% lapply(hmda_api_gofer) %>% rbindlist() %>%
-    setDT() %>% setnames("county", "fips") %>% setkey(fips)
-  rs %<>% .[county_lookup, county:=county_name] %>%
-    dcast(year + county + races ~ actions_taken, value.var="count") %>%
-    .[, denial_share:=(`3`/`1`)] %>% setnames(c(`3`,`1`),c("denial_count","full_count"))           # Keep counts for context
-  return(rs)
-}
+                       "Free Form Text Only") 
 
 ## Meg start -----------------------------------------
 
@@ -121,7 +49,7 @@ kitsap_clean$received <- as.numeric(as.character(kitsap_clean$received))
 
 # reorganize data and include data year
 kitsap_clean$share_denials <- kitsap_clean$denied/kitsap_clean$received
-pierce_clean$data_year <- 2021
+kitsap_clean$data_year <- 2021
 kitsap_clean$region <- "kitsap"
 kitsap_clean <- kitsap_clean[c(6, 7, 1, 2, 3, 4, 5)]
 
@@ -147,7 +75,7 @@ king_clean$received <- as.numeric(as.character(king_clean$received))
 
 # reorganize data and include data year
 king_clean$share_denials <- king_clean$denied/king_clean$received
-pierce_clean$data_year <- 2021
+king_clean$data_year <- 2021
 king_clean$region <- "king"
 king_clean <- king_clean[c(6, 7, 1, 2, 3, 4, 5)]
 
@@ -177,16 +105,13 @@ region_clean <- rbind(kitsap_clean, king_clean, pierce_clean)
 
 # plot data -----------------------------------------
 
-bar_chart <- static_bar_chart(t=just_region_11,
-                              x="fact_value", y="focus_attribute_ord",
-                              fill="tenure",
-                              est = "percent",
-                              pos= "stack",
-                              color = "psrc_light", 
-                              title = paste0(indicator_title, ": King (2011)"),
-                              subtitle = "by equity focus group",
-                              source = "U.S. Census Bureau, American Community Survey (ACS) 5-Year Public Use Microdata Sample (PUMS)")
-bar_chart
+facet_chart <- static_facet_line_chart(t=region_clean,
+                        x="share_denials", y="race_ethnicity",
+                        fill="race_ethnicity", facet="median_income",
+                        color="psrc_light",
+                        est ="percent",
+                        scales="fixed")
+facet_chart
 
 # Export data
 
