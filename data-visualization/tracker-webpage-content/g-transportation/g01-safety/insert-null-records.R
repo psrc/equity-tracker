6d2b29dc0c58d7505e044bc873c009d784843683
cyyllam <cyyllam@gmail.com>
2023-11-01 12:13:38 -0700

    revised comments

--- data-visualization/tracker-webpage-content/g-transportation/g01-safety/insert-null-records.R
+++ data-visualization/tracker-webpage-content/g-transportation/g01-safety/insert-null-records.R
@@ -14,9 +14,11 @@ load(file = file.path(base_dir, theme_dir, ind_dir, "rda-data", rda))
 # insert null records ----
 standard_base_year <- 2010
 
+# main table
 df_base <- data_clean %>% 
   mutate(data_year_num = as.numeric(data_year))
 
+# set-up columns to identify missing years by the unique categories/geography
 d <- df_base %>% 
   group_by(county, equity_group, quintile, county_ord, equity_group_ord, quintile_ord) %>% 
   summarise(num_unique_years = length(unique(data_year)),
@@ -24,14 +26,14 @@ d <- df_base %>%
             end_year = as.numeric(max(unique(data_year)))) %>% 
   mutate(base_year = standard_base_year)
 
-# identify missing years by county & quintile
 # create list-column, a vector of missing years in a cell
 d_calc <- d %>% 
   rowwise() %>% 
   mutate(missing_years = ifelse(start_year == base_year, NA, list(seq(base_year, (start_year-1))))) 
 
+# unpack so each missing year is a row of its own
 d_unnest <- d_calc %>% 
-  unnest(missing_years) # unpack so each missing year is a row of its own
+  unnest(missing_years) 
 
 # create table with null records for chart
 df_nulls <- d_unnest %>% 
@@ -47,22 +49,22 @@ df <- bind_rows(df_base, df_nulls) %>%
 
 ## set variables
 
-x = "data_year" 
-y = "estimate"
-y_min = 0
-y_max = 100
-color = "purples"
-geo = "county_ord"
-fill = "quintile_ord" 
-facet = "equity_group_ord"
-dec = 1
-esttype = "number"
+x <- "data_year" 
+y <- "estimate"
+y_min <- 0
+y_max <- 100
+color <- "purples"
+geo <- "county_ord"
+fill <- "quintile_ord" 
+facet <- "equity_group_ord"
+dec <- 1
+esttype <- "number"
 
-num_colors = 5
-color_rev = FALSE
-title = "Traffic Related Deaths and <br> Serious Injuries per 100,000"
-subtitle = "Census Tract Level"
-source = paste("Washington Traffic Safety Commission", 
+num_colors <- 5
+color_rev <- FALSE
+title <- "Traffic Related Deaths and <br> Serious Injuries per 100,000"
+subtitle <- "Census Tract Level"
+source <- paste("Washington Traffic Safety Commission", 
                "U.S. Census Bureau, American Community Survey (ACS) 2020 5-Year Public Use Microdata Sample (PUMS)", sep="\n")
 
 line_chart <- equity_tracker_line_facet(df = df,
