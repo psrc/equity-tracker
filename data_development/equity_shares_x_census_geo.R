f549384ff7af437a7ec2574053cbd043af06b6b1
jensenmj <mjensen@psrc.org>
2024-05-21 10:52:49 -0700

    Handle all backyears without commenting stuff out. 8^)

--- data_development/equity_shares_x_census_geo.R
+++ data_development/equity_shares_x_census_geo.R
@@ -6,16 +6,33 @@ library(data.table)
 # Functions ----------------------------------------------
 
 # ACS variables
-vars <- c("B03002_001","B03002_003",                                                               # POC / race
-          "C17002_001","C17002_008",                                                               # income
-          "B22010_001","B22010_004","B22010_007",                                                  # disability
-          "B11005_001","B11005_002",                                                               # presence of youth in household
-          "B11007_001","B11007_002",                                                               # presence of older in household
-          "C16002_001","C16002_004","C16002_007","C16002_010","C16002_013"                         # limited English proficiency
-         )
+choose_acs_varids <- function(dyear){
+  var_ids <- c("B03002_001","B03002_003",                                                          # POC / race
+               "C17002_001","C17002_008",                                                          # income
+               "B11005_001","B11005_002",                                                          # presence of youth in household
+               "B11007_001","B11007_002")                                                          # presence of older in household
+  if(dyear>2011){
+    var_ids %<>% c(c("B22010_001","B22010_004","B22010_007"))                                      # disability - 2012+
+  }
+  if(dyear>2015){
+    var_ids %<>% c(c("C16002_001","C16002_004","C16002_007","C16002_010","C16002_013"))            # limited English proficiency - 2016+
+  }
+return(var_ids)
+}
+
+choose_dims_n_vars <- function(dyear){
+  equity_dims   <- c("poc","income",
+                     "youth","older")
+  if(dyear>2011){ equity_dims %<>% c("disability") }
+  if(dyear>2015){ equity_dims %<>% c("lep") }
+  df <- data.frame(dims=equity_dims,
+                   share_vars <- paste0(equity_dims,"_share"),
+                   quintile_vars <- paste0(equity_dims,"_quintile"))
+return(df)
+}
 
 cty_codes <- data.table( fips=c("033","035","053","061"),
-                       county=c("King","Kitsap","Pierce","Snohomish"))
+                         county=c("King","Kitsap","Pierce","Snohomish"))
 
 label_quintile <- function(var){
   breakpoints <- unique(quantile(var, probs=0:5/5, na.rm=TRUE))                                    # Unique so all-zero quintiles act as one bin
@@ -25,33 +42,34 @@ label_quintile <- function(var){
 }
 
 get_psrc_equity_shares <- function(dyear, entirety){                                               # dyear - last year of 5yr ACS;
-  equity_dims   <- c("poc","income","disability",                                                  # entirety - either "region" or "county"
-                     "youth","older","lep"
-                     )
-  share_vars <- paste0(equity_dims,"_share")
-  quintile_vars <- paste0(equity_dims,"_quintile")
+                                                                                                   # entirety - either "region" or "county"
+  dimvar_df <- choose_dims_n_vars(dyear)
 
-  shares <- get_acs(geography="tract", variables=vars, state=53,                                   # Retrieves the data
+  shares <- get_acs(geography="tract", variables=choose_acs_varids(dyear), state=53,               # Retrieves the data
                     county=cty_codes$fips, year=dyear, survey="acs5") %>%
     setDT %>% dcast(GEOID ~ variable, value.var="estimate") %>%
     .[B03002_001 > 0,`:=`(Year=..dyear, county=stringr::str_sub(GEOID,3,5),
                           poc_share= (B03002_001 - B03002_003) / B03002_001,                       # Calculate share per block group
                           income_share= (C17002_001 - C17002_008) / C17002_001,
-                          disability_share= (B22010_001-B22010_004-B22010_007) / B22010_001,
                           youth_share= B11005_002 / B11005_001,
-                          older_share= B11007_002 / B11007_001,
-                          lep_share= (C16002_004 + C16002_007 + C16002_010 + C16002_013) / C16002_001
-                          )] %>%
-    .[, which(grepl("\\d$", colnames(.))):=NULL] %>%
-    .[, lapply(.SD, function(x) replace(x, is.nan(x), NA))] %>% .[!is.na(Year)]                    # Replace NaN
-  if(entirety=="county"){
-    shares[county=='033', (quintile_vars):=lapply(.SD, label_quintile), .SDcols=share_vars]        # Add county-level quintile value
-    shares[county=='035', (quintile_vars):=lapply(.SD, label_quintile), .SDcols=share_vars]
-    shares[county=='053', (quintile_vars):=lapply(.SD, label_quintile), .SDcols=share_vars]
-    shares[county=='061', (quintile_vars):=lapply(.SD, label_quintile), .SDcols=share_vars]
+                          older_share= B11007_002 / B11007_001
+                          )]
+  if(dyear>2011){
+    shares[B03002_001 > 0,`:=`(disability_share= (B22010_001-B22010_004-B22010_007) / B22010_001)]
+    }
+  if(dyear>2015){
+    shares[B03002_001 > 0,`:=`(lep_share= (C16002_004 + C16002_007 + C16002_010 + C16002_013) / C16002_001)]
+  }
+  shares %<>% .[, which(grepl("\\d$", colnames(.))):=NULL] %>%
+       .[, lapply(.SD, function(x) replace(x, is.nan(x), NA))] %>% .[!is.na(Year)]                 # Replace NaN
+  if(entirety=="county"){                                                                          # Add county-level quintile value
+    shares[county=='033', (dimvar_df$quintile_vars):=lapply(.SD, label_quintile), .SDcols=dimvar_df$share_vars]
+    shares[county=='035', (dimvar_df$quintile_vars):=lapply(.SD, label_quintile), .SDcols=dimvar_df$share_vars]
+    shares[county=='053', (dimvar_df$quintile_vars):=lapply(.SD, label_quintile), .SDcols=dimvar_df$share_vars]
+    shares[county=='061', (dimvar_df$quintile_vars):=lapply(.SD, label_quintile), .SDcols=dimvar_df$share_vars]
     shares %<>% .[cty_codes, county:=county, on =.(county=fips)]
   }else if(entirety=="region"){
-    shares[, (quintile_vars):=lapply(.SD, label_quintile), .SDcols=share_vars]                     # Add regional quintile value
+    shares[, (dimvar_df$quintile_vars):=lapply(.SD, label_quintile), .SDcols=dimvar_df$share_vars] # Add regional quintile value
     shares[, county:="region"]
   }
   shares %<>% setnames(old=c("GEOID","Year"), new=c("geoid", "data_year")) %>%
@@ -63,8 +81,8 @@ write_shares_to_elmer <- function(df){
                      "USING stg.tract_shares AS source",
                      "ON target.data_year = source.data_year AND target.geoid = source.geoid AND target.county = source.county",
                      "WHEN MATCHED THEN UPDATE SET",
-                     paste0(paste0(share_vars,"=source.", share_vars, collapse=", "), ", ",
-                            paste0(quintile_vars,"=source.", quintile_vars, collapse=", ")),
+                     paste0(paste0(dimvar_df$share_vars,"=source.", dimvar_df$share_vars, collapse=", "), ", ",
+                            paste0(dimvar_df$quintile_vars,"=source.", dimvar_df$quintile_vars, collapse=", ")),
                      "WHEN NOT MATCHED BY TARGET THEN INSERT (", paste0(colnames(tract_shares), collapse=", "),")",
                      "VALUES (", paste0("source.", colnames(tract_shares), collapse=", "), ");")
 
@@ -82,7 +100,7 @@ write_shares_to_elmer <- function(df){
 # fwrite(equity_shares, filename)
 
 # Write to Elmer
-args <- expand.grid(years= c(2022), entirety=c("county","region"))
+args <- expand.grid(years= c(2009:2022), entirety=c("county","region"))
 tract_shares <- list()
-tract_shares <- mapply(get_psrc_equity_shares, args$years, args$entirety, SIMPLIFY = FALSE) %>% data.table::rbindlist()
-write_shares_to_elmer(tract_shares)
+tract_shares <- mapply(get_psrc_equity_shares, args$years, args$entirety, SIMPLIFY = FALSE)
+lapply(tract_shares, write_shares_to_elmer)
